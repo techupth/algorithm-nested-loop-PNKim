feb01b85e4b7e8310a29483d04d1bff4e5c4f42d
PNKim <paljapon.nait@gmail.com>
2024-07-03 11:40:55 +0700

    feat: resolve ex-3

--- ex-1.js
+++ ex-1.js
@@ -3,7 +3,7 @@ function printPattern(x) {
   let result = "";
   for (let i = 1; i <= x; i++) {
     for (let j = 1; j <= i; j++) {
-      result += j + " ";
+      result += j + "\t";
     }
     result += "\n";
   }
--- ex-3.js
+++ ex-3.js
@@ -1,4 +1,13 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function multiplicationTable() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function multiplicationTable(x) {
+  let result = "";
+  for (let i = 1; i <= x; i++) {
+    for (let j = 1; j <= x; j++) {
+      result += j * i + "\t";
+    }
+    result += "\n";
+  }
+  return result;
 }
+
+console.log(multiplicationTable(5));
