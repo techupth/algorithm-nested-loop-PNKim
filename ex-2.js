8cae2b28f5b89b33770b2c9c2dc79fc3f9d1a2d4
PNKim <paljapon.nait@gmail.com>
2024-07-03 11:35:49 +0700

    feat: resolve ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,4 +1,13 @@
-// เริ่มเขียนโค้ดตรงนี้ 
-function printStar() {
-   
+// เริ่มเขียนโค้ดตรงนี้
+function printStar(x) {
+  let result = "";
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j <= i; j++) {
+      result += "*";
+    }
+    result += "\n";
+  }
+  return result;
 }
+
+console.log(printStar(5));
